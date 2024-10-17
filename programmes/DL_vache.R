7c0fb390c7a7ed6d8a27b811be02edec409a824e
Erwan Gouhier <erwan.gouhier@agrocampus-ouest.fr>
2024-10-17 22:54:13 +0200

    Perceptron à couches multiples finaux pour img0

--- programmes/DL_vache.R
+++ programmes/DL_vache.R
@@ -1,10 +1,18 @@
+library(data.table)
+library(reticulate)
+library(keras)
+library(glue)
+library(magick)
 library(keras)
 library(tensorflow)
-library(data.table)
-
+library(imager)
+library(dplyr)
 #On utilise les features pour les img0 du jeu de données pour gagner du temps de calcul
+dta <- fread("donnees/dataset.csv")
 dta1 <- fread("donnees/data_img0.csv")
 # dta1 <- dta1[-nrow(dta1),]
+
+dta1 <- cbind(dta1, height_in_inch=dta$height_in_inch)
 
 #On extrait les features et on les normalise
 features <- as.matrix(dta1[,-c(1:3)])
@@ -18,7 +26,9 @@ num_classes <- length(unique(class))
 #On passe au format 0,1,2,3 pour le réseau de neurones
 labels_cat <- to_categorical(class -1, num_classes = num_classes)
 
+
 #On sépare le jeu de données en train_set et test_set (80/20)
+set_random_seed(245)
 set.seed(245)
 indices <- sample(1:nrow(dta1), size = 0.8 * nrow(dta1)) # 80% pour l'entraînement
 
@@ -28,20 +38,22 @@ train_label <- labels_cat[indices,]
 test_features <- features[-indices,]
 test_label <- labels_cat[-indices,]
 
+colSums(train_label)
+colSums(test_label)
 
 #On définie l'architecture du modèle
 model_1 <- keras_model_sequential() %>%
   layer_dense(units = 128,input_shape = c(ncol(features)))%>%
   layer_activation_leaky_relu() %>%
-  layer_dropout(rate=0.4) %>% 
+  layer_dropout(rate=0.5) %>% 
   layer_dense(units = 64) %>%
-  layer_dropout(rate=0.4) %>% 
+  layer_dropout(rate=0.5) %>% 
   layer_dense(units = num_classes, activation = 'softmax')
 
 #On définie la fonction de perte et le critère d'optimisation
 model_1 %>% compile(
   loss = 'categorical_crossentropy',  
-  optimizer = optimizer_adam(learning_rate = 0.0001), 
+  optimizer = optimizer_adam(learning_rate = 0.001), 
   metrics = c('accuracy')
 )
 
@@ -65,6 +77,7 @@ cat("Test Loss:", results["loss"], "\n")
 cat("Test Accuracy:", results["accuracy"], "\n")
 
 
+
 #Réseau de neurones pour prédire le poids d'une vache à partir des features de VGG_16----
 
 dta1 <- fread("donnees/data_img0.csv")
@@ -79,6 +92,7 @@ features <- scale(features)
 # Division des données en ensembles d'entraînement et de test
 #On prédéfini une seed et on sépare les données en 80% entrainement et 20% test
 set.seed(123)
+set_random_seed(123)
 indices <- sample(1:nrow(dta1), size = 0.8 * nrow(dta1)) # 80% pour l'entraînement
 
 train_features <- features[indices,]
@@ -91,15 +105,15 @@ test_target <- target[-indices]
 model <- keras_model_sequential() %>%
   layer_dense(units = 256,input_shape = ncol(features)) %>% 
   layer_activation_leaky_relu() %>%
-  layer_dropout(rate = 0.6) %>%
+  layer_dropout(rate = 0.5) %>%
   layer_dense(units = 64,activation="relu") %>%
-  layer_dropout(rate = 0.6) %>%
+  layer_dropout(rate = 0.5) %>%
   layer_dense(units = 1)  # Couche de sortie avec 1 unité (prédiction d'une valeur continue)
 
 #On définit la fonction de perte et le critère d'optimisation
 model %>% compile(
   loss = "mse",  #La fonction de perte est l'erreur absolue moyenne
-  optimizer = optimizer_adam(learning_rate = 0.0001),
+  optimizer = optimizer_adam(learning_rate = 0.008),
   metrics = c("mean_absolute_error")  # On mesure l'erreur absolue moyenne
 )
 
@@ -109,7 +123,7 @@ history <- model %>% fit(
   y = train_target,
   epochs = 300,
   batch_size = 64,
-  validation_split = 0.2,  
+  validation_split = 0.3,  
   callbacks = list(
     callback_early_stopping(monitor = "val_loss", patience = 10),  # Arrêt précoce si pas d'amélioration
     callback_reduce_lr_on_plateau(monitor = "val_loss", factor = 0.5, patience = 5)  # Réduction du learning rate si plateau
